33e0367d1bdd90949cc70424ed8c0df12051880b
bjergsenson <malondonoc@icloud.com>
2023-11-15 07:57:23 -0500

    Update Location.js

--- src/components/Location.js
+++ src/components/Location.js
@@ -8,19 +8,18 @@ const Location = () => {
         dispatch({
             type: 'CHG_CURRENCY',
             payload: val,
-        })
-    }
-
+        });
+    };
 
     return (
-        <div className='alert alert-secondary'> Location {
-            <select name="Location" id="Location" onChange={event => changeLocation(event.target.value)}>
+        <div className='alert alert-success'>
+            Location{' '}
+            <select className="custom-select bg-success text-white" name="Location" id="Location" onChange={(event) => changeLocation(event.target.value)}>
                 <option value="£">£ Pound</option>
-                <option value="₹">₹ Ruppee</option>
+                <option value="₹">₹ Rupee</option>
                 <option value="€">€ Euro</option>
                 <option value="USD">$ Dollar</option>
             </select>
-        }
         </div>
     );
 };
